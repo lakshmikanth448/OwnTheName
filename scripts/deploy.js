1f1c62e289ab5aacde668fd7636063612ea2d285
lakshmikanth448 <132090524+lakshmikanth448@users.noreply.github.com>
2026-02-23 10:51:24 +0530

    Initial commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -11,10 +11,27 @@ const tokens = (n) => {
 }
 
 async function main() {
+  const [deployer] = await ethers.getSigners()
+  const NAME = "ETH Daddy"
+  const SYMBOL = "ETHD"
+  const  ETHDaddy = await ethers.getContractFactory("ETHDaddy")
+  const ethDaddy = await ETHDaddy.deploy(NAME, SYMBOL)
+  await ethDaddy.deployed();
+  console.log(`Deployed Domain Contract at: ${ethDaddy.address}\n`)
+
+  const names=["jack.eth", "johneth", "henry.eth", "cobalt.eth", "oxygen.eth", "carbon.eth"]
+  const costs=[tokens(10),tokens(25),tokens(15),tokens(2.5),tokens(3),tokens(1)]
+  
+  for(var i=0; i<6; i++){
+    const transaction = await ethDaddy.connect(deployer).list(names[i], costs[i])
+    await transaction.wait()
+    console.log(`Listed domain ${names[i+1]}: ${costs[i]}`)
+  }
 
 }
-
+ 
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+ 
