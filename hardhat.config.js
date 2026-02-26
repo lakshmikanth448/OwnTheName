0ffa875a06afd1e352235169d835a04ec486a587
lakshmikanth448 <132090524+lakshmikanth448@users.noreply.github.com>
2026-02-26 13:30:32 +0000

    Add deployment configuration and build optimization

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY || "9aa3d95b3bc440fa88ea12eaa4456161"}`,
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111
+    }
+  }
 };
